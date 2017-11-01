336acdd3bc74951d12b63a064be57d0e79321454
Victor Verbitsky <vektor@krista.ru>
2017-11-01 17:06:12 +0300

    Обновление скрипта сборки. По умолчанию  происходит только локальная сборка. Для того, чтобы отправить собраный образ на bintray.org нужно указать ключ -r Если при сборке не надо вытягивать базовый образ, то необходимо указать опцию -p

--- build.sh
+++ build.sh
@@ -1,21 +1,60 @@
 #!/bin/bash -e
 
-VERSION=1.0.java8
-DATE=$(date +"%Y-%m-%d")
+function build() {
+    local release
+    release="dry"
+    doPull="yes"
 
-IMAGE_TAG="javister-docker-docker.bintray.io/javister/javister-docker-openjdk"
-PROXY_ARGS="--build-arg http_proxy=${http_proxy} \
-            --build-arg no_proxy=${no_proxy}"
-docker pull javister-docker-docker.bintray.io/javister/javister-docker-base:1.0
+    while getopts ":rph" opt; do
+        case $opt in
+            r)
+                release="release"
+                ;;
+            p)
+                doPull="no"
+                ;;
+            h)
+                cat <<EOF
+usage: build [OPTION]... [-- [docker build opts]]
+  -h        show this help.
+  -r        push resulting image.
+  -p        don't pull base image.
+EOF
+                return 0;
+                ;;
+            :)
+                echo "$0: option requires an argument -- '$OPTARG'" 1>&2
+                return 1
+                ;;
+            *)
+                echo "$0: invalid option -- '$OPTARG'" 1>&2
+                return 1
+                ;;
+        esac
+    done
+    shift $((OPTIND-1))
 
-docker build \
-    --tag ${IMAGE_TAG}:latest \
-    --tag ${IMAGE_TAG}:${VERSION} \
-    --tag ${IMAGE_TAG}:${VERSION}-${DATE} \
-    ${PROXY_ARGS} \
-    $@ \
-    .
+    VERSION=1.0.java8
+    DATE=$(date +"%Y-%m-%d")
 
-docker push ${IMAGE_TAG}:latest
-docker push ${IMAGE_TAG}:${VERSION}
-docker push ${IMAGE_TAG}:${VERSION}-${DATE}
+    IMAGE_TAG="javister-docker-docker.bintray.io/javister/javister-docker-openjdk"
+    PROXY_ARGS="--build-arg http_proxy=${http_proxy} \
+                --build-arg no_proxy=${no_proxy}"
+    [ "${doPull}" == "yes" ] && docker pull javister-docker-docker.bintray.io/javister/javister-docker-base:1.0
+
+    docker build \
+        --tag ${IMAGE_TAG}:latest \
+        --tag ${IMAGE_TAG}:${VERSION} \
+        --tag ${IMAGE_TAG}:${VERSION}-${DATE} \
+        ${PROXY_ARGS} \
+        $@ \
+        .
+
+    [ "${release}" == "release" ] && docker push ${IMAGE_TAG}:latest
+    [ "${release}" == "release" ] && docker push ${IMAGE_TAG}:${VERSION}
+    [ "${release}" == "release" ] && docker push ${IMAGE_TAG}:${VERSION}-${DATE}
+}
+
+trap "exit 1" INT TERM QUIT
+
+build "$@"

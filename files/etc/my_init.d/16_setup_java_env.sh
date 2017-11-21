c694ac3c31dbd1572c4931bafcdb4793e15a402f
Victor Verbitsky <vektor@krista.ru>
2017-11-21 18:20:05 +0300

    Изменение уровня логгирования версии Java

--- files/etc/my_init.d/16_setup_java_env.sh
+++ files/etc/my_init.d/16_setup_java_env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+[ "$LOG_LEVEL" == "DEBUG" ] && echo -n " -showversion " >> /etc/container_environment/JVM_OPTS
+
 # Если переменная установлена в пустое значение - то значит настраивать прокси не надо и выходим
 [ -z "$PROXY" ] && exit 0
 
@@ -7,4 +9,4 @@ echo -n "-Dhttp.proxyHost=$PROXY_HOST -Dhttp.proxyPort=$PROXY_PORT -Dhttps.proxy
 
 [ "$JAVA_NON_PROXY" ] && echo -n "-Dhttp.nonProxyHosts=$JAVA_NON_PROXY " >> /etc/container_environment/JVM_OPTS
 
-[ "$PROXY_USER" ] && echo -n "-Dhttp.proxyUser=$PROXY_USER -Dhttp.proxyPassword=$PROXY_PASS -Dhttps.proxyUser=$PROXY_USER -Dhttps.proxyPassword=$PROXY_PASS " >> /etc/container_environment/JVM_OPTS
+[ "$PROXY_USER" ] && echo -n "-Djdk.http.auth.tunneling.disabledSchemes= -Dhttp.proxyUser=$PROXY_USER -Dhttp.proxyPassword=$PROXY_PASS -Dhttps.proxyUser=$PROXY_USER -Dhttps.proxyPassword=$PROXY_PASS " >> /etc/container_environment/JVM_OPTS
